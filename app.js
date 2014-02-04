ab7511ae3a0482e9d2543ea2db45713e67161ecd
Tommy Doyle <tdoyle@andrew.cmu.edu>
2014-02-04 00:48:30 -0500

    Kind of working... But not really...

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var sass = require('node-sass');
 var secrets = require('./secrets')
-var foursquare = (require('foursquarevenues'))(secrets.clientid,secrets.clientkey);
 
-console.log(secrets.clientid,secrets.clientkey)
+var foursquare = (require('foursquarevenues'))(secrets.clientid,secrets.clientkey);
 
 var app = express();
 
@@ -24,6 +24,13 @@ app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+ sass.middleware({
+     src: __dirname + '/sass/', //where the sass files are 
+     dest: __dirname + '/public/css/', //where css should go
+     debug: false // obvious
+ })
+);
 
 // development only
 if ('development' == app.get('env')) {
@@ -33,6 +40,10 @@ if ('development' == app.get('env')) {
 app.get('/', function (req,res) {
 	res.sendfile('views/home.html')
 });
+
+// app.get('/public/*', function (req,res) {
+// 	res.sendfile(req.path)
+// });
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
