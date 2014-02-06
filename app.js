47bda24c08563bb636afd1aef25473cd5fc4ea8a
Tommy Doyle <tdoyle@andrew.cmu.edu>
2014-02-06 15:44:26 -0500

    Gave up on getting the sass-middleware to work, doing it live

--- app.js
+++ app.js
@@ -24,13 +24,6 @@ app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(
- sass.middleware({
-     src: __dirname + '/sass/', //where the sass files are 
-     dest: __dirname + '/public/css/', //where css should go
-     debug: false // obvious
- })
-);
 
 // development only
 if ('development' == app.get('env')) {
@@ -40,10 +33,6 @@ if ('development' == app.get('env')) {
 app.get('/', function (req,res) {
 	res.sendfile('views/home.html')
 });
-
-// app.get('/public/*', function (req,res) {
-// 	res.sendfile(req.path)
-// });
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
